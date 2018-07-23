81b2bfe9e15aa8548d5192f361708bd5816b7cf1
Ben Cole <ben@colecode.co.uk>
2018-07-23 20:47:20 +0100

    setup home module

--- src/app/home/containers/home-container/home-container.component.ts
+++ src/app/home/containers/home-container/home-container.component.ts
@@ -3,20 +3,15 @@ import { Http, Response } from '@angular/http';
 import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home-container-component',
   templateUrl: 'home-container.component.html' 
 })
-export class HomeComponent implements OnInit {
+export class HomeContainerComponent implements OnInit {
   public message: string;
 
   constructor(private http: Http) {}
 
   ngOnInit() {
     this.message = 'Hello';
-    this.http.get('/api/ok')
-      .pipe(map((res: Response) => res.json()))
-      .subscribe(data => {
-        console.log(data);
-      });
   }
 }
