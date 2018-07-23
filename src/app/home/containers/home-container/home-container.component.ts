a717d2c5ab72a2f4caf54323e8548ad0b6b24e4b
Ben Cole <ben@colecode.co.uk>
2018-07-23 21:27:57 +0100

    language list and test

--- src/app/home/containers/home-container/home-container.component.ts
+++ src/app/home/containers/home-container/home-container.component.ts
@@ -1,17 +1,47 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'home-container-component',
-  templateUrl: 'home-container.component.html' 
+    selector: 'home-container-component',
+    templateUrl: 'home-container.component.html'
 })
 export class HomeContainerComponent implements OnInit {
-  public message: string;
+    public message: string;
 
-  constructor(private http: Http) {}
+    languageList = [
+        {
+            name: 'javascript',
+            icon: 'devicon-javascript-plain'
 
-  ngOnInit() {
-    this.message = 'Hello';
-  }
+        },
+        {
+            name: 'typescript',
+            icon: 'devicon-typescript-plain'
+        },
+        {
+            name: 'angular',
+            icon: 'devicon-angularjs-plain'
+
+        },
+        {
+            name: 'sass',
+            icon: 'devicon-sass-original'
+        },
+        {
+            name: 'angular js',
+            icon: 'devicon-angularjs-plain-wordmark'
+        },
+        {
+            name: 'css',
+            icon: 'devicon-css3-plain'
+        }
+    ];
+
+
+    constructor(private http: Http) { }
+
+    ngOnInit() {
+        this.message = 'Hello';
+    }
 }
