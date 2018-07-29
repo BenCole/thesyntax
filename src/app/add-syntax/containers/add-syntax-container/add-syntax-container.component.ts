90d219560132f9121a27e6872b2e680959f8c1f5
Ben Cole <ben@colecode.co.uk>
2018-07-29 19:29:14 +0100

    added reactive add syntax form

--- src/app/add-syntax/containers/add-syntax-container/add-syntax-container.component.ts
+++ src/app/add-syntax/containers/add-syntax-container/add-syntax-container.component.ts
@@ -12,19 +12,41 @@ export class AddSyntaxContainerComponent implements OnInit {
     text: string = '// Add syntax here';
     syntax = <Syntax>{};
     addSyntaxForm: FormGroup;
+    submitAttempted: boolean = false;
+    syntaxError: boolean;
+
+    get name() { return this.addSyntaxForm.get('name'); }
+
+    get category() { return this.addSyntaxForm.get('category'); }
+
+    submitForm(form) {
+        // reset from potential previous submit
+        this.submitAttempted = false;
+        this.syntaxError = false; 
+
+        if (form.status === 'INVALID') {
+            this.submitAttempted = true;
+            return;
+        }
+        // lack of form builder support with ng ace :(
+        if (this.text === '// Add syntax here' || !this.text ) {
+            this.syntaxError = true;
+            return;
+        }
+        console.log(form.value);
+        console.log(this.text);
+    }
 
     constructor(private fb: FormBuilder) { }
 
     ngOnInit() {
-        // this.addSyntaxForm = new FormGroup({
-        //     name: new FormControl('', Validators.required)
-        // });
         this.addSyntaxForm = this.fb.group({
-            name: ['', [Validators.required]]
+            name: ['', [Validators.required]],
+            category: ['', [Validators.required]]
         });
 
-        this.addSyntaxForm.patchValue({
-            name: 'Ben'
-        });
+        // this.addSyntaxForm.patchValue({
+        //     name: 'Ben'
+        // });
     }
 }
