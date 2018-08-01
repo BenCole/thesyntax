8a67dbe98599e2a9ff3749b6fcc7e4e80e704d28
Ben Cole <ben@colecode.co.uk>
2018-08-01 12:57:56 +0100

    wip

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ const DIST_FOLDER = join(process.cwd(), 'dist');
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
 
 // * NOTE :: leave this as require() since this file copied on server start
-const apiRoutes = require('./dist/api-server/routes/api-routes');
+const apiRoutes = require('./dist/api-server/routes/api-routes.js');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
@@ -35,10 +35,56 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
-// Example Express Rest API endpoints/
-// app.get('/api/**', (req, res) => { });
-// After allllll that above middleware, we finally handle our own routes!
-app.use('/api/', apiRoutes);
+// app.use('/api/v1.0/', apiRoutes);
+
+
+// ******************************
+
+// const bodyParser = require('body-parser');
+
+// const mongoose = require('mongoose');
+
+// // Make sure we are running node 7.6+
+// const [major, minor] = process.versions.node.split('.').map(parseFloat);
+// if (major < 7 || (major === 7 && minor <= 5)) {
+//   console.log('Please go to nodejs.org and download version node 7.6 or greater. 👌\n ');
+//   process.exit();
+// }
+
+// // import environmental variables from our variables.env file
+// require('dotenv').config({ path: './api-server/.env' });
+
+// // Connect to our Database and handle any bad connections
+// console.log(process.env.DATABASE);
+// mongoose.connect(process.env.DATABASE);
+// mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+// mongoose.connection.on('error', (err) => {
+//   console.error(`🚫 🚫 🚫 🚫 → ${err.message}`);
+// });
+
+// const language = require('./api-server/models/Language');
+// const syntax = require('./api-server/models/syntax');
+
+// // Express server
+
+// // Takes the raw requests and turns them into usable properties on req.body
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// // Syntax.create({ name: 'for loop', syntax: 'forrrrr', language: '5b60758f3b8802462fd32b19' }, function (err, awesome_instance) {
+// //   if (err) return handleError(err);
+// //   // saved!
+// // });
+
+// app.use('/api/v1.0', apiRoutes);
+
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
+// **********************
 
 // Server static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
--- src/app/add-syntax/add-syntax.module.ts
+++ src/app/add-syntax/add-syntax.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { AceEditorModule } from 'ng2-ace-editor';
+// import { AceEditorModule } from 'ng2-ace-editor';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddSyntaxContainerComponent } from './containers/add-syntax-container/add-syntax-container.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        AceEditorModule,
         FormsModule,
         ReactiveFormsModule,
         RouterModule.forChild([
